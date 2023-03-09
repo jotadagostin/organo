9f9c2eb6c82497091721f13b1a1385ee19fa2c35
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-09 23:16:55 +0100

    list

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,14 +1,26 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+  const times = [
+    "Programacao",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovacao e Gestao",
+  ];
+
   return (
     <section className="formulario">
       <form>
-        <h2>Preencha os dados para criar  o card do colaborador</h2>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereco da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
       </form>
     </section>
   );
