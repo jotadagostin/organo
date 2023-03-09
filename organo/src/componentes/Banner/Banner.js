90fd09876760811ee0d35423a586ef5e8d6d348d
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-09 11:50:22 +0100

    setting the banner
    with css as well

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+// JSX
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+    <img src="/imgs/banner.png" alt="banner principal da pagina Organo" />
+  </header>
+  )
+  
+}
+
+export default Banner;
