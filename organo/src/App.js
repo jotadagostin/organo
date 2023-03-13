dff8f463e3e73c6da492e873ac2d5f40242fbb6c
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-13 21:12:58 +0100

    Update App.js

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,21 +1,62 @@
-import { useState } from 'react';
-import Banner from './componentes/Banner';
-import Formulario from './componentes/Formulario';
-import Time from './componentes/Time';
+import { useState } from "react";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 
 function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "#57c278",
+      corSecundaria: "#d9f7e9",
+    },
+    {
+      nome: "Front-End",
+      corPrimaria: "#82cffa",
+      corSecundaria: "#e8f8ff",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "#a6d157",
+      corSecundaria: "#f0f8e2",
+    },
+    {
+      nome: "Devops",
+      corPrimaria: "#e06b69",
+      corSecundaria: "fde7e8",
+    },
+    {
+      nome: "UX e Design",
+      corPrimaria: "#db6ebf",
+      corSecundaria: "#fae9f5",
+    },
+    {
+      nome: "Mobile",
+      corPrimaria: "#ffba05",
+      corSecundaria: "#fff5d9",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#ff8a29",
+      corSecundaria: "#ffeedf",
+    },
+  ];
 
-  const [colaboradores, setColaboradores] = useState([])
+  const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
-    setColaboradores([...colaboradores, colaborador])
-  }
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  };
 
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
       <Time nome="Programacao" />
       <Time nome="FrontEnd" />
       <Time nome="Data Science" />
