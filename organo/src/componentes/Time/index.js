05a436a0b1cc9d904823620c51e0d98c33f7fcea
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-14 11:38:32 +0100

    colors
    seting the colors in the cards

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -11,6 +11,7 @@ const Time = (props) => {
         <div className="colaboradores">
           {props.colaboradores.map((colaborador) => (
             <Colaborador
+              corDeFundo={props.corPrimaria}
               key={colaborador.nome}
               nome={colaborador.nome}
               cargo={colaborador.cargo}
