3f15be03dd61806e9b43745d5cb4cf4ea4897de3
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-14 10:20:52 +0100

    componentes
    index..js and css

---
+++ organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div>
+      <div>
+        <img src="https://github.com/viniciosneves.png" alt="" />
+        <div>
+          <h4>vinicious Neves</h4>
+          <h5>Instrutor</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
